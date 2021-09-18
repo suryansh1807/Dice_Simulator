7207b0d85cebaa760d44c3109f80bc268ab8714a
Suryansh Singh <69665093+suryansh1807@users.noreply.github.com>
2021-09-18 19:24:25 +0530

    Add files via upload

---
+++ dice_simulator.py
@@ -0,0 +1,49 @@
+import random
+
+rolled = random.randint(1,6)
+
+if rolled == 1:
+        print(" _____")
+        print("|     |")
+        print("|     |")
+        print("|  0  |")
+        print("|     |")
+        print("|_____|")
+if rolled == 2:
+        print(" _____")
+        print("|     |")
+        print("| 0   |")
+        print("|     |")
+        print("|   0 |")
+        print("|_____|")
+if rolled == 3:
+        print(" _____")
+        print("|     |")
+        print("|     |")
+        print("|0 0 0|")
+        print("|     |")
+        print("|_____|")
+if rolled == 4:
+        print(" _____")
+        print("|     |")
+        print("|0   0|")
+        print("|     |")
+        print("|0   0|")
+        print("|_____|")
+if rolled == 5:
+        print(" _____")
+        print("|     |")
+        print("|0   0|")
+        print("|  0  |")
+        print("|0   0|")
+        print("|_____|")
+if rolled == 6:
+        print(" _____")
+        print("|     |")
+        print("|0 0 0|")
+        print("|     |")
+        print("|0 0 0|")
+        print("|_____|")            
+
+
+
